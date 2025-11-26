81f11f860aa7d97ceb4f7b13e15daf742735b491
amansinghl <aman.singh@vamaship.com>
2025-11-26 16:59:50 +0530

    feat: adding rto placeholder page

---
+++ app/(auth)/shipments/rto/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
+      To get started, edit the page.tsx file.
+      <br />
+      <Button>Hello World</Button>
+    </h1>
+  );
+}
